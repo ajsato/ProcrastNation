bc5aea8d331b7b984751b24eca296f2e964cf8f5
Andrew Lundholm <andrewatmadison@gmail.com>
2017-02-18 14:41:22 -0600

    Setup Angular Material

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 (function () {
     firebase.initializeApp(config);
+
+    var app = angular.module('procrastNation', ['firebase', 'ngMaterial']);
+
+    app.controller('procrastNationController',
+        ["$firebaseArray", function ($firebaseArray) {
+
+            var self = this;
+
+        }]);
+
+
 })();
