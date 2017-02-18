3f543648ec69d76b37a36400cce74b5b11611840
Saloni Saraf <ssaraf3@wisc.edu>
2017-02-18 17:04:55 -0600

    Added Recent Activities

--- main.js
+++ main.js
@@ -7,6 +7,28 @@
         ["$firebaseArray", function ($firebaseArray) {
 
             var self = this;
+            //Create the recent activities
+            self.activities = [{
+                what: "Activity 1",
+                when: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                notes: "Description"
+            }, {
+                what: 'Activity 2',
+                when: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                notes: "Description"
+            }, {
+                what: 'Activity 3',
+                when: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                notes: "Description"
+            }, {
+                what: 'Activity 4',
+                when: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                notes: "Description"
+            }, {
+                what: 'Activity 5',
+                when: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                notes: "Description"
+            }];
 
         }]);
 
