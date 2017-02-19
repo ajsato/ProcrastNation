b2912dec6ecc831363b1a797bef256110d88f45f
Andrew Lundholm <andrewatmadison@gmail.com>
2017-02-18 18:32:06 -0600

    Display dialog to enter a username when user is new

--- main.js
+++ main.js
@@ -4,15 +4,34 @@
     var app = angular.module('procrastNation', ['firebase', 'ngMaterial', 'googlechart']);
 
     app.controller('procrastNationController',
-        ["$firebaseArray", "$firebaseAuth", function ($firebaseArray, $firebaseAuth) {
+        ["$firebaseArray", "$firebaseAuth", "$firebaseObject", "$mdDialog", function ($firebaseArray, $firebaseAuth, $firebaseObject, $mdDialog) {
 
             var self = this;
 
             var firebaseUser = null;
             var auth = $firebaseAuth();
-
             auth.$signInWithPopup("google").then(function (result) {
+
+                firebaseUser = $firebaseObject(firebase.database().ref().child(result.user.uid));
                 console.log("Signed in as:", result.user.uid);
+
+                firebaseUser.$loaded(function (data) {
+                    if (!firebaseUser.username) {
+                        var confirm = $mdDialog.prompt()
+                            .title('Hey new user!')
+                            .textContent('Please enter a username')
+                            .placeholder('Username')
+                            .ariaLabel('Username')
+                            .ok('Okay!')
+                            .cancel('Cancel');
+
+                        $mdDialog.show(confirm).then(function (result) {
+                            firebaseUser.username = result;
+                            firebaseUser.$save();
+                        }, function () {
+                        });
+                    }
+                });
             }).catch(function (error) {
                 console.error("Authentication failed:", error);
             });
